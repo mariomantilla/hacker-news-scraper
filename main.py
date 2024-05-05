45c8807f7631bae726eb8e991e529070c8c3a866
Mario Mantilla <mariamantillasanchez@gmail.com>
2024-05-05 04:40:40 +0200

    Added crawling and scrapping to main

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from parsing import parse_news_data
+from web_crawling import get_source_code
+
+
 def main():
-    pass
+    html = get_source_code('https://news.ycombinator.com/')
+    parsed_data = parse_news_data(html)
+    return parsed_data
 
 if __name__ == "__main__":
-    main()
+    data = main()
+    print(data)
